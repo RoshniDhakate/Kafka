d2e259d36eea185efd6b76cc5706b49df85c2024
RoshniDhakate <rdhakate02@gmail.com>
2022-08-11 17:25:12 +0530

    Create KafKaConsumer1.java

---
+++ demo/src/main/java/com/demo/example/consumer/KafKaConsumer1.java
@@ -0,0 +1,18 @@
+package com.demo.example.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafKaConsumer1 {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafKaConsumer1.class);
+
+    @KafkaListener(topics = "testTopic1",
+                    groupId = "group_id1")
+    public void consume(String message){
+        LOGGER.info(String.format("Message received -> %s", message));
+    }
+}
